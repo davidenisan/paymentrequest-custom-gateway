783919f5646d8ca6704557384a15c0132366e479
David Enisan <davidenisan@gmail.com>
2020-10-07 13:51:59 +0100

    Update payment-methods.js

--- api/payment-methods.js
+++ api/payment-methods.js
@@ -1,30 +1,43 @@
-import axios from 'axios'
+// import axios from 'axios'
 
-export default async (req, res) => {
-  console.log(JSON.stringify(req.body))
-  if (req.body && req.body.publicToken) {
-    try {
-      // Validate the request was made by Snipcart
-      await axios.get(`${process.env.PAYMENT_URL}/api/public/custom-payment-gateway/validate?publicToken=${req.body.publicToken}`)
+// export default async (req, res) => {
+//   console.log(JSON.stringify(req.body))
+//   if (req.body && req.body.publicToken) {
+//     try {
+//       // Validate the request was made by Snipcart
+//       await axios.get(`${process.env.PAYMENT_URL}/api/public/custom-payment-gateway/validate?publicToken=${req.body.publicToken}`)
 
-      // Return the payment methods
-      return res.json([{
-        id: 'sleeky_pay',
-        name: 'SleekyPay',
-        checkoutUrl: `https://sleeky-pay.netlify.app/index.html`,
-      },{
-        id: 'paymentrequest-custom-gateway',
-        name: 'Google pay',
-        checkoutUrl: process.env.CHECKOUT_URL,
-        iconUrl: `${process.env.CHECKOUT_URL}/google_pay.png`
-      }])
-    }catch(e){
-      // Couldn't validate the request
-      console.error(e)
-      return res.status(401).send()
-    }
-  }
+//       // Return the payment methods
+//       return res.json([{
+//         id: 'sleeky_pay',
+//         name: 'SleekyPay',
+//         checkoutUrl: `https://sleeky-pay.netlify.app/index.html`,
+//       },{
+//         id: 'paymentrequest-custom-gateway',
+//         name: 'Google pay',
+//         checkoutUrl: process.env.CHECKOUT_URL,
+//         iconUrl: `${process.env.CHECKOUT_URL}/google_pay.png`
+//       }])
+//     }catch(e){
+//       // Couldn't validate the request
+//       console.error(e)
+//       return res.status(401).send()
+//     }
+//   }
 
-  // No publicToken provided. This means the request was NOT made by Snipcart
-  return res.status(401).send()
-}
+//   // No publicToken provided. This means the request was NOT made by Snipcart
+//   return res.status(401).send()
+// }
+fetch('https://payment.snipcart.com/api/public/custom-payment-gateway/validate?publicToken=${request.PublicToken}', {
+  headers: { "Content-Type": "application/json; charset=utf-8" },
+  method: 'POST',
+  let paymentMethodList = [{
+    id: 'sleeky_pay',
+    name: 'SleekyPay',
+    checkoutUrl: `https://sleeky-pay.netlify.app/index.html`,
+}]
+ return {
+        statusCode: 200,
+        body: JSON.stringify(paymentMethodList)
+    };
+})
